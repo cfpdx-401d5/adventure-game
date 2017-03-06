07e21156af5121a21178a5df776cc8e32d356f82
sdcaulley <stephanie.caulley@gmail.com>
2017-03-06 05:30:04 -0800

    trying to lift state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const nextMove = require('./plot.js');
 //import './index.css';
 
 function Choices(props) {
-    console.log('choices props: ', props);
+    console.log('choice props: ', props);
     const buttons = props.value.map(item => {
         return (<li key={item.id}>
-            <button id={item.id}>{item.name}</button>
+            <button id={item.id} onClick={() => props.onClick(item.id)}>{item.name}</button>
         </li>);
     });
     return (
@@ -16,14 +16,13 @@ function Choices(props) {
 }
 
 class Story extends React.Component {
-    render (props) {
-        console.log('PROPS', this.props);
+    render(props) {
         return (
         <div>
             <p id={this.props.id}>{this.props.plot}</p>
             <img src={this.props.img} alt='from the Princess Bride'/>
             <ul>
-                <Choices value={this.props.choices} />
+                <Choices value={this.props.choices} onclick={() => this.props.onClick()} />
             </ul>
         </div>
         );
@@ -34,23 +33,30 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            statePlot: nextMove(4)
-        }
+            statePlot: nextMove(props.start)
+        };
     }
+
+    handleClick(value) {
+        this.setState({
+            statePlot: nextMove(value)
+        });
+    }
+
     render() {
         const plot = this.state.statePlot.plot;
         const id = this.state.statePlot.id;
         const img = this.state.statePlot.image;
         const choices = this.state.statePlot.choices;
-        console.log('state', this.state);
         return (
         <div>
-            <Story plot={plot} id={id} img={img} choices={choices} />
+            <Story plot={plot} id={id} img={img} choices={choices} onClick={() => this.handleClick()} />
         </div>
-    );}
+        );
+    }
 }
 
 ReactDOM.render(
-  <Game />,
+  <Game start={4}/>,
   document.getElementById('root')
 );
