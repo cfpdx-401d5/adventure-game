1927df14e77df46a97e6fe807583476f69bdf280
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-08 16:44:46 -0800

    push to switch repos

--- src/index.js
+++ src/index.js
@@ -1,106 +1,184 @@
-import React, { Component } from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var userAnswerArr = [];
-var answerArr = ['A', 'B', 'B'];
-var idx = 0;
-
-const rooms = [
+let rooms = [
   {
-    roomName: 'Room1',
-    message: 'You are in this room1 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
+    roomName: 'Selection Room 1',
+    message: ['You are in a dark room.', 'Scary, huh?', 'It probably doesn\'t help that you know you\'ll soon encounter a monster.', 'Better pick up one of these items to help you defeat it.'],
+    buttonA: {
+      name: 'GOLD',
+      image: '',
+    },
+    buttonB: {
+      name: 'COOKIE',
+      image: '',
+    }
   },
   {
-    roomName: 'Room2',
-    message: 'You are in this room2 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
-  },
-  {
-    roomName: 'Room3',
-    message: 'You are in this room3 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
-  },
-  {
-    roomName: 'FinalRoom',
-    message: 'Did you win?',
-    buttonA: 'Fight monster',
-    buttonB: 'Run away'
+    roomName: 'Selection Room 2',
+    message: ['Dude.', 'It\'s still dark and still scary.', 'Good thing you freed up your other hand.', 'Choose another item to take with you to the next room.'],
+    buttonA: {
+      name: 'SWORD',
+      image: '',
+    },
+    buttonB: {
+      name: 'TEDDY BEAR',
+      image: '',
+    }
   },
 ];
 
-function MakeRoom(props) {
-  console.log('idx in mkrom', idx);
+class Room extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageNumber: 0,
+      currentMessage: props.room.message[0],
+      lastMessage: false,
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._timer);
+  }
+  componentDidMount() {
+    this.messageDisplay();
+  }
+
+  messageDisplay() {
+    this._timer = setInterval(() => {
+      const newNumber = this.state.messageNumber + 1;
+      const newMessage = this.props.room.message[newNumber]
+      if (this.state.messageNumber === 3) {
+        this.setState({
+          lastMessage: true,
+        })
+        clearInterval(this._timer);
+      } else {
+        this.setState({
+          messageNumber: newNumber,
+          currentMessage: newMessage
+        })
+      }
+    }, 1800);
+  }
+
+  render() {
+    if (!this.state.lastMessage){
+      return (
+        <div>
+          <p>{this.props.room.message[this.state.messageNumber]}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>{this.props.room.message[this.state.messageNumber]}</p>
+          < SelectionBar 
+            buttonA={this.props.room.buttonA} 
+            buttonB={this.props.room.buttonB} 
+            onSelection={this.props.onSelection}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+function SelectionBar(props) {
+  console.log('props',props)
   return (
     <div>
-      <p>{props.rooms[this.state.idx].message}</p>
-      <Button value={props.room.buttonA} />
-      <Button value={props.room.buttonB} />
+      <button onClick={(e) => props.onSelection(e, props.buttonA)}>{props.buttonA.name}</button>
+      <button onClick={(e) => props.onSelection(e, props.buttonB)}>{props.buttonB.name}</button>
     </div>
   );
 }
 
 function WinOrLose(props) {
-  console.log(props.decision);
-  if (props.decision) {
-    return <p>You win!</p>;
+  if (props.hasWon === false) {
+    return (
+      <div>
+        <p>You picked the wrong item! You did not defeat the monster!</p>
+      </div>
+    )
   }
   else {
-    return <p>You lose.</p>;
+    return (
+      <div>
+        <p>Good thing you didn't eat that cookie! You've made the Cookie Monster very happy!!!</p>
+      </div>
+    )
   }
 }
 
-class Button extends React.Component {
+class GameApp extends React.Component {
+  
   constructor(props) {
     super(props);
-    this.state = { hasWon: true };
-    console.log('constructor', this.state.hasWon)
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { 
+      hasWon: false,
+      roomNumber: 0,
+      selections: [],
+      message: props.rooms[0].message,
+    };
+    this.onSelection = this.onSelection.bind(this);
   }
 
-  handleClick(e) {
+  onSelection(e, selected) {
     e.preventDefault();
-    
-    userAnswerArr.push(this.props.value);
-    
-    if (userAnswerArr[idx] !== answerArr[idx]) { this.setState({ hasWon: false }) }
-    console.log("userAnsArr", userAnswerArr[idx])
-    console.log("AnsArr", answerArr[idx])
-    console.log(this.state.hasWon)
-    idx++;
 
+    let newSelections = this.state.selections.slice();
+    newSelections.push(selected);
 
-    if(idx < answerArr.length) {
-      // ReactDOM.render(
-      //   <MakeRoom />,
-      //   document.getElementById('root')
-      // );
+    let roomNumber = this.state.roomNumber + 1;
+    let roomCount = this.props.rooms.length;
+    let message;
 
-    } else if (idx === answerArr.length) {
-      // ReactDOM.render(
-      //   <MakeFinalRoom />,
-      //   document.getElementById('root')
-      // );
-
-    } else {
-      // ReactDOM.render(
-      //   <WinOrLose decision={this.state.hasWon} />,
-      //   document.getElementById('root')
-      // );
+    if (roomNumber < roomCount) {
+      message = rooms[roomNumber].message;
+    } else if (roomNumber === roomCount) {
+      message = ['You can hear the monster in the next room.', 'Eeek!', 'You don\'t have enough energy to carry both items with you.', 'Choose the one item you think with defeat the monster.'];
     }
-  };
+
+    this.setState({
+      roomNumber,
+      selections: newSelections,
+      hasWon: newSelections.length === 3 && selected.name === 'COOKIE' ? true : false,
+      message,
+    });
+  }
 
   render() {
-    return <button onClick={this.handleClick}>Choose {this.props.value}</button>;
-  };
+    let roomNumber = this.state.roomNumber;
+    let roomCount = this.props.rooms.length;
+    if(roomNumber > roomCount) {
+      return (
+        <WinOrLose hasWon={this.state.hasWon}/>
+      )
+    }
+
+    let room = rooms[roomNumber]
+    if (!room) { 
+      room = {
+        buttonA: this.state.selections[0],
+        buttonB: this.state.selections[1],
+      }
+    }
+
+    //move selectionbar to room component
+    //pass room, key, onSelection to Room as props
+    return (
+      <div>
+        < Room key={this.state.roomNumber} room={room} onSelection={this.onSelection}/>
+      </div>
+    );
+  }
 }
 
 
 ReactDOM.render(
-    <Button rooms={rooms}/>,
-    document.getElementById('root')
-  );
+  <GameApp rooms={rooms} />,
+  document.getElementById('root')
+);
