8672adf7dc5547be471e9df93ab68f0ffea8c6b5
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-22 17:23:15 -0700

    corrects logic for winning assembles new object for setState

--- src/index.js
+++ src/index.js
@@ -2,111 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/*var userAnswerArr = [];
-var answerArr = ['A', 'B', 'B'];
-var idx = 0;
-
-const rooms = [
-  {
-    roomName: 'Room1',
-    message: 'You are in this room1 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
-  },
-  {
-    roomName: 'Room2',
-    message: 'You are in this room2 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
-  },
-  {
-    roomName: 'Room3',
-    message: 'You are in this room3 and your choices are: A || B.',
-    buttonA: 'A',
-    buttonB: 'B'
-  },
-  {
-    roomName: 'FinalRoom',
-    message: 'Did you win?',
-    buttonA: 'Fight monster',
-    buttonB: 'Run away'
-  },
-];
-
-function MakeRoom(props) {
-  console.log('idx in mkrom', idx);
-  return (
-    <div>
-      <p>{props.rooms[this.state.idx].message}</p>
-      <Button value={props.room.buttonA} />
-      <Button value={props.room.buttonB} />
-    </div>
-  );
-}
-
-function WinOrLose(props) {
-  console.log(props.decision);
-  if (props.decision) {
-    return <p>You win!</p>;
-  }
-  else {
-    return <p>You lose.</p>;
-  }
-}
-
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasWon: true };
-    console.log('constructor', this.state.hasWon)
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    
-    userAnswerArr.push(this.props.value);
-    
-    if (userAnswerArr[idx] !== answerArr[idx]) { this.setState({ hasWon: false }) }
-    console.log("userAnsArr", userAnswerArr[idx])
-    console.log("AnsArr", answerArr[idx])
-    console.log(this.state.hasWon)
-    idx++;
-
-
-    if(idx < answerArr.length) {
-      // ReactDOM.render(
-      //   <MakeRoom />,
-      //   document.getElementById('root')
-      // );
-
-    } else if (idx === answerArr.length) {
-      // ReactDOM.render(
-      //   <MakeFinalRoom />,
-      //   document.getElementById('root')
-      // );
-
-    } else {
-      // ReactDOM.render(
-      //   <WinOrLose decision={this.state.hasWon} />,
-      //   document.getElementById('root')
-      // );
-    }
-  };
-
-  render() {
-    return <button onClick={this.handleClick}>Choose {this.props.value}</button>;
-  };
-}
-
-
-ReactDOM.render(
-    <Button rooms={rooms}/>,
-    document.getElementById('root')
-  );
-}*/
-
-
 class Game extends Component {
   constructor(props) {
     super(props);
@@ -137,7 +32,7 @@ class Game extends Component {
         {
           roomName: 'FinalRoom',
           message: 'Did you win?',
-          buttonA: 'Fight monster',
+          buttonA: 'Fight Monster',
           buttonB: 'Run away'
         },
       ],
@@ -145,37 +40,29 @@ class Game extends Component {
     this.clickHandler = this.clickHandler.bind(this);
   }
 
-  clickHandler(clickValue) {
-    // e.preventDefault();
-    console.log('clickv', clickValue);
-    
-    console.log('before click', this.state.idx);
+  clickHandler(clickValue) {    
+    let newSetStateObject = {};
 
     const newPlayerArray = this.state.playerArray.concat(clickValue);
-    const newIdx = this.state.idx + 1;
-    // let updatedArr = this.state.playerArray.push(clickValue);
-    let newSetStateObject = { idx: newIdx, playerArray: newPlayerArray };
-    // this.setState({ idx: newIdx, playerArray: newPlayerArray });
-    // this.state.playerArray.push(clickValue);
-    console.log('playerArr is', newPlayerArray);
-    // console.log('after click', this.state.idx);
-
-    // if (this.state.playerArray[this.state.idx] !== this.state.answerArray[this.state.idx]) {
-    //   this.setState({ hasWon: false });
-    // }
-    if (newSetStateObject.playerArray[newSetStateObject.idx] !== this.state.answerArray[newSetStateObject.idx]) {
-      newSetStateObject.hasWon = false;
+  
+    if (newPlayerArray.length <= this.state.answerArray.length){
+      if (newPlayerArray[this.state.idx] !== this.state.answerArray[this.state.idx]) {
+        newSetStateObject.hasWon = false;
+      }
     }
-    console.log('hasWon state', this.state.hasWon);
+    const newIdx = this.state.idx + 1;
+    newSetStateObject.idx = newIdx;
+    newSetStateObject.playerArray = newPlayerArray;
     this.setState(newSetStateObject);
-
   }
 
   render() {
     if (this.state.idx < 4) {
+      // console.log('make room number', this.state.idx);
       return <MakeRoom clickHandler={this.clickHandler} room={this.state.rooms} index={this.state.idx} />;
     } else {
-      return <WinOrLosePage result={this.hasWon}/>;
+      // console.log('this.state.idx is ', this.state.idx, this.state.hasWon);
+      return <WinOrLosePage result={this.state.hasWon}/>;
     } 
   }
 }
